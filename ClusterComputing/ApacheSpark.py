2682d7f34eaad3344802305266ad8325a56506de
MislavJaksic <jaksicmislav@gmail.com>
2017-03-05 17:37:11 +0100

    Added Cluster Computing
    In preparation for an interview, I found out how complicated it is to
    handle extermely large (more then a milliard) datasets.
    
    
    Former-commit-id: 956529c47a16eca0a6dcdaa532c2bd811192b244
    Former-commit-id: b098182a865d8ad4fb05ed6c4cf5e7e5af90755e

---
+++ ClusterComputing/ApacheSpark.py
@@ -0,0 +1,42 @@
+#You can run a Spark Python script with "spark-2.1.0-bin-hadoop2.7\bin\spark-submit ApacheSpark.py"
+from pyspark import SparkConf, SparkContext
+
+CLUSTER_URL = "local"
+APP_NAME = "MyApp"
+
+#Initialise Spark
+conf = SparkConf().setMaster(CLUSTER_URL).setAppName(APP_NAME)
+sc = SparkContext(conf = conf)
+
+#Create a RDD
+lines = sc.parallelize(["first line", "second line"])
+#OR
+fileLines = sc.textFile("NUMBERS.txt")
+
+#.persist() command will load the RDD and keep it in memory
+
+#Transformations (return RDDs):
+#.map() #.flatMap() #.filter()
+#.distinct() #.union() #.intersection() #subtract() #.cartesian() #-> set operations
+numbers = fileLines.map(lambda x: int(x))
+evenNumbers = numbers.filter(lambda x: (x%2 == 0))
+words = lines.flatMap(lambda line: line.split(" "))
+
+#Actions (don't return RDDs)
+#.reduce() #.fold() #.aggregate() #.count() #.first()
+words.first() #-> first
+
+for line in evenNumbers.take(5):
+  print line
+#Sum all the elements
+print (evenNumbers.reduce(lambda x,y: x + y)) #-> 90
+
+#Column 1: startingValue=0, doInNode=x+1, combineNodesData=x+y
+#Column 2: startingValue=0, doInNode=x+y, combineNodesData=x+y
+sumAndCount = evenNumbers.aggregate((0,0),
+                                    lambda x,y: (x[0] + 1, x[1] + y),
+									lambda x,y: (x[0] + y[0], x[1] + y[1]))
+print sumAndCount[0] #-> 9
+print sumAndCount[1] #-> 90
+
+sc.stop()
---
+++ ClusterComputing/README.txt
@@ -0,0 +1,4 @@
+Introduction to Apache Spark
+============================
+
+Apache Spark allows a computation to be performed across many different computers at the same time. It abstracts data by creating a Resilient Distributed Dataset (RDD) and allows the user to perform actions and transformations on it. It also abstract the computer cluster through the Spark Context.
