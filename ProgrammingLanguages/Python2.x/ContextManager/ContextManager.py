16c9c9aedd56ae74f564aefc6a2629b9c109f377
MislavJaksic <jaksicmislav@gmail.com>
2017-09-24 09:55:49 +0200

    Class Context Manager
    Former-commit-id: 79096acd3f51eb9bca037a07e675bd73146e390c
    Former-commit-id: f4940fb8e537d683576187d503915a64fbda99a1

--- ProgrammingLanguages/Python2.x/ContextManager/ContextManager.py
+++ ProgrammingLanguages/Python2.x/ContextManager/ContextManager.py
@@ -1,19 +1,44 @@
-class SequenceClass(object):
-  def __init__(self):
-    self.value = 10
+class Bee(object):
+  
+  def __init__(self, role):
+    self.role = role
+  
+  def GetRole(self):
+    return self.role
+
+class ManagerClass(object):
+  
+  def __init__(self, beeRole):
+    self.beeRole = beeRole
   
   def __enter__(self):
     print "Opened"
+    self.aBee = Bee(self.beeRole)
+    return self.aBee
   
   def __exit__(self, exc_type, exc_value, traceback):
     print "Closed"
 	
-with SequenceClass() as anObject:
+with ManagerClass("Queen") as queenBee:
   print "Created an object"
+  print queenBee.GetRole()
+  
+#The following with be outputted:
+#Opened
+#Created an object
+#Queen
+#Closed
+#Exception: I'm causing trouble
+
+print
+with ManagerClass("Worker") as workerBee:
+  print "Created an object"
+  print workerBee.GetRole()
   raise Exception("I'm causing trouble")
   
 #The following with be outputted:
 #Opened
 #Created an object
+#Worker
 #Closed
 #Exception: I'm causing trouble
