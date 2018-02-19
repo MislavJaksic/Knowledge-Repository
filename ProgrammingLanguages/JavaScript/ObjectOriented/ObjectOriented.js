717f1485775c7685f3556e9a15e4801bfc4310ba
MislavJaksic <jaksicmislav@gmail.com>
2018-02-19 09:31:57 +0100

    Cracked the JavaScript inheritance
    Former-commit-id: 89387c13d6055d4a34c9c46c9d4b02b3ede01198
    Former-commit-id: 1b974eb4f8af1e50d167d5265ebb8bb550230af7

--- ProgrammingLanguages/JavaScript/ObjectOriented/ObjectOriented.js
+++ ProgrammingLanguages/JavaScript/ObjectOriented/ObjectOriented.js
@@ -33,10 +33,49 @@ rareHoney.writeProperties() //-> purple, 3
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Details_of_the_Object_Model
 
 
+//Demonstrated proper inheritance similar to that in truely object oriented languages
 
+function ParentObject(parent_value){
+  this.parent_value = parent_value;
+};
 
+ParentObject.prototype.ImpliedMethod = function() {
+  alert("Parent implied " + this.parent_value);
+};
 
+ParentObject.prototype.OverridenMethod = function() {
+  alert("Parent overriden " + this.parent_value);
+};
 
+ParentObject.prototype.AlteredMethod = function() {
+  alert("Parent altered " + this.parent_value);
+};
 
+function ChildObject(parent_value, child_value){
+  ParentObject.call(this, parent_value);
+  this.child_value = child_value;
+};
 
+ChildObject.prototype = Object.create(ParentObject.prototype);
+ChildObject.prototype.constructor = ChildObject;
 
+ChildObject.prototype.OverridenMethod = function() {
+  alert("Child overriden " + this.child_value);
+};
+
+ChildObject.prototype.AlteredMethod = function() {
+  ParentObject.prototype.AlteredMethod.call(this)
+  alert("Child altered " + this.child_value);
+  ParentObject.prototype.AlteredMethod.call(this)
+};
+
+parent = new ParentObject(2);
+
+child = new ChildObject(3, 5);
+
+parent.ImpliedMethod(); //-> 2
+parent.OverridenMethod(); //-> 2
+
+child.ImpliedMethod(); //-> 3
+child.OverridenMethod(); //-> 5
+child.AlteredMethod(); //-> 3 5 3
