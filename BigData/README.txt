8c9d0e9bd81b525b4cc5acf535aa42191f455f97
MislavJaksic <jaksicmislav@gmail.com>
2018-07-29 21:59:39 +0200

    Sorted the research notes
    Completed the much needed sorting of all the research notes.
    
    
    Former-commit-id: 8cd3a6efcd206b212df30a5767a227131b7def62
    Former-commit-id: 307f0c6e2cb761f0762b0da19557e7dd5a11430c

--- BigData/README.txt
+++
@@ -1,7 +0,0 @@
-## Hive Streaming
-
-https://community.hortonworks.com/articles/49949/test-7.html
-
-Examples: https://github.com/mfjohnson/HiveStreamingExamples
-
-JavaDoc API: https://hive.apache.org/javadocs/r1.2.2/api/overview-summary.html
---
+++ Linux/Scipts/elif.sh
@@ -0,0 +1,13 @@
+#! /bin/ bash
+pom="hm"
+if [ pom="kul" ]
+then
+  echo "Linux je $pom"
+elif [ pom="bezveze" ]
+then
+  echo "Linux masterrace"
+else
+  echo "Linux je OS intelektualne elite"
+fi
+# Output: Linux je hm
+# Why??? Souldn't it be Linux je OS intelektualne elite""
