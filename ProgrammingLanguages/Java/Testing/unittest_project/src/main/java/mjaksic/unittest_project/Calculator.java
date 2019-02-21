49c7e1f01bef306217ecdf1cc90878187064c928
MislavJaksic <jaksicmislav@gmail.com>
2019-02-21 18:26:07 +0100

    Improved JUnit5 example
    Former-commit-id: 0131f081770d69e905a5762437d09575deb067d5
    Former-commit-id: 1c47422152f9d35a647bd708bb778e4a21f7a210

--- ProgrammingLanguages/Java/Testing/unittest_project/src/main/java/mjaksic/unittest_project/Calculator.java
+++ ProgrammingLanguages/Java/Testing/unittest_project/src/main/java/mjaksic/unittest_project/Calculator.java
@@ -1,9 +1,19 @@
 package mjaksic.unittest_project;
 
 public class Calculator {
-
-	public int add(int a, int b) {
-		return a + b;
+	private int last_result;
+	
+	public int Add(int a, int b) {
+		int result = a + b;
+		this.SetResult(result);
+		return result;
 	}
-
+	
+	public int GetResult() {
+		return this.last_result;
+	}
+	
+	public void SetResult(int result) {
+		this.last_result = result;
+	}
 }
